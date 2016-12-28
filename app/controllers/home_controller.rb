4932e03f0eb4d8be1d31ba69c3a1e71ae9795220
tuuk79 <stevecyoo@gmail.com>
2016-12-28 14:25:35 -0600

    adding home controller to return json as poc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+      @todos = { :status => "yay json!" }
+
+      render json: @todos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root 'home#index'
   get 'home/index'
+  
+namespace :api do
+ resources :index, :defaults => { :format => 'json' }
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
