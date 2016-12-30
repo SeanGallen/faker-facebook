8e15f8cf18f696b93c6b52b00d75646e7d975dff
tuuk79 <stevecyoo@gmail.com>
2016-12-30 15:20:33 -0600

    guklp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -7,21 +7,25 @@ gulp.task('default', function() {
 });
 
 gulp.task('copy-files-to-public', function() {
-    console.log('copying javascript files to public/Scripts folder');
-    gulp.src('./src/*.js')
-        .pipe(gulp.dest('./public/Scripts'));
+    console.log('copying app.js files to public folder');
+    gulp.src('./src/app.js')
+        .pipe(gulp.dest('./public'));
+
+    // console.log('copying javascript files to public/Scripts folder');
+    // gulp.src('./src/*.js')
+    //     .pipe(gulp.dest('./public/Scripts'));
 
     console.log('copying src files to public');
     gulp.src('./src/*.html')
         .pipe(gulp.dest('./public'));
 
-    console.log('copying necessary third party js files to public');
-    gulp.src('./node_modules/angular/angular.js')
-        .pipe(gulp.dest('./public/scripts'));
+    // console.log('copying necessary third party js files to public');
+    // gulp.src('./node_modules/angular/angular.js')
+    //     .pipe(gulp.dest('./public/scripts'));
 
-    // console.log('copying node modules to public');
-    // gulp.src('./node_modules/**')
-    //     .pipe(gulp.dest('./public/node_modules'));
+    console.log('copying node modules to public');
+    gulp.src('./node_modules/**')
+        .pipe(gulp.dest('./public/node_modules'));
 });
 
 gulp.task('delete-public', function() {
--- client/public/scripts/person.js
+++
@@ -1,5 +0,0 @@
-class Person {
-	constructor() {}
-}
-
-module.exports = Person;
