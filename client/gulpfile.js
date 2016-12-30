4205367df66767ef2393bdf1a6e069c5a882ed02
tuuk79 <stevecyoo@gmail.com>
2016-12-30 14:48:45 -0600

    configuring gulp to copy files

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
+var vinylPaths = require('vinyl-paths');
+var del = require('del');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-console.log('copying src files to public');
-gulp.src('./src/*')
-    .pipe(gulp.dest('./public'));
+gulp.task('copy-files-to-public', function() {
+    console.log('copying javascript files to public/scripts folder');
+    gulp.src('./src/*.js')
+        .pipe(gulp.dest('./public/scripts'));
 
-console.log('copying node modules to public');
-gulp.src('./node_modules/**')
-    .pipe(gulp.dest('./public/node_modules'));
+    console.log('copying src files to public');
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('./public'));
+
+    console.log('copying node modules to public');
+    gulp.src('./node_modules/**')
+        .pipe(gulp.dest('./public/node_modules'));
+});
+
+gulp.task('delete-public', function() {
+    console.log('deleting files from public folder');
+    gulp.src('./public/*')
+        .pipe(vinylPaths(del));
+});
