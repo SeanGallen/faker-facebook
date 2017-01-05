73fd9bf22c3146b5100d44ce947bacac7c6b07d6
tuuk79 <stevecyoo@gmail.com>
2017-01-04 20:18:58 -0600

    removing unnecessary recipe files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :recipes
   root 'home#angular'
   get 'home/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,5 @@ ActiveRecord::Schema.define(version: 20170102221905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recipes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
