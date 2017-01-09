b6224696a356f98460f9c97c0e0c73d1df3ea212
Sean <sjgallen@gmail.com>
2017-01-08 22:12:05 -0600

    routes now have a resource for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'home#login'
   get 'home/index'
-  get 'users/index'
 
-  # resources :users
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
