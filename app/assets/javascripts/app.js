2b1a51e7ca7ff0bb0bdf7214f8baa76e44348e37
tuuk79 <stevecyoo@gmail.com>
2017-01-11 23:09:06 -0600

    using angular directive to display partial views instead of rails

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,19 @@
-angular.module('fakerApp', [])
-.controller('MainCont', [
+var app = angular.module('fakerApp', []);
+
+app.controller('MainCont', [
     '$scope',
     function($scope){
       $scope.facebook = "Hello Faker!";
     }]);
+
+app.directive('newUsers', function() {
+  return {
+    templateUrl: 'new_users.html'
+  };
+});
+
+app.directive('login', function() {
+  return {
+    templateUrl: 'login.html'
+  };
+});
